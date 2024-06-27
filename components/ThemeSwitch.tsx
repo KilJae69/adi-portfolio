07898cefcaf3e8b7e9954aa64eb7036c40ffd5d2
KilJae69 <adi.toromanovic@outlook.com>
2024-06-27 15:21:38 +0200

    updated CV

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -3,12 +3,12 @@
 import { useThemeContext } from "@/context/themeContext";
 import { BsMoon, BsSun } from "react-icons/bs";
 
-
 export default function ThemeSwitch() {
- const  {theme, toggleTheme} = useThemeContext();
+  const { theme, toggleTheme } = useThemeContext();
 
   return (
     <button
+      aria-label="Toggle theme"
       onClick={toggleTheme}
       className="fixed flex items-center justify-center bottom-5 right-5 size-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] rounded-full border border-opacity-40 shadow-2xl border-white bg-white hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
     >
